aa077f6b572d4a3f5268489a94dac40ffec08551
Yariserrano12 <yari12_94@hotmail.com>
2023-07-17 13:26:13 -0600

    componentes

---
+++ src/componentes/contador/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "../contador/styles"; 
+
+import React from 'react'
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
+
+
+
+
+
+
+
+
+
+
+
+export default
